9c6b7a57d038b7c6efa78bb890e75cdc81471887
Mebin Abraham <mebin95+work@gmail.com>
2020-03-19 11:09:43 +0000

    revert pot files

--- scripts/translate_schemas.py
+++ scripts/translate_schemas.py
@@ -33,7 +33,8 @@ def translate_schemas(runner_directory):
             output_dir = f"{runner_directory}/schemas/{language}"
             language_dir = f"{runner_directory}/translations/{language}"
 
-            logger.info("Building %s/%s", relative_dir, schema_file)
+            logger.info("\n-------"
+                        "Building %s/%s", relative_dir, schema_file)
 
             os.makedirs(relative_dir, exist_ok=True)
 
