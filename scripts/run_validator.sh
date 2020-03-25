0226e62e2cac0eccee80d563b8b31414e8715bad
James Gardiner <james@jgardiner.co.uk>
2020-03-25 15:23:33 +0000

    Fix incorrect validator branch

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-branch=custom-section-summaries
+branch=master
 docker pull onsdigital/eq-questionnaire-validator:$branch
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$branch"
