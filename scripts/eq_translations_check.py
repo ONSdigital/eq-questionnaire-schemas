409c1a018291f02d2f591e3483870efe1c8936cd
petechd <piotr.chudziak@ons.gov.uk>
2020-04-01 11:21:47 +0100

    Add version check

--- scripts/eq_translations_check.py
+++ scripts/eq_translations_check.py
@@ -1,29 +1,24 @@
 import logging
 import requests
-import re
 import sys
+
+import eq_translations
+
 from requests.exceptions import ConnectionError
 
 logger = logging.getLogger(__name__)
+version = f"v{eq_translations.__version__}"
 
 try:
-    response = requests.get("https://api.github.com/repos/ONSdigital/eq-translations/commits/master").json()
-    commit_sha = response.get('version')
+    response = requests.get("https://api.github.com/repos/ONSdigital/eq-translations/releases").json()
+    latest_tag = response[0]['tag_name']
 
-    f = open("Pipfile.lock", "r")
-    file = f.readlines()
-    for i, line in enumerate(file):
-        if '"eq-translations"' in line:
-            ref = re.compile(r"\w{40}")
-            mo = ref.search(file[i + 3])
-            pipfile_sha = mo.group()
-            if pipfile_sha == commit_sha:
-                logger.error("eq-translations up to date")
+    if version == latest_tag:
+        logger.error("eq-translations up to date")
 
-            else:
-                logger.error('Newer version of eq-translations available, use "pipenv update"')
-                sys.exit(1)
-            break
+    else:
+        logger.error('Newer version of eq-translations available, use "pipenv update"')
+        sys.exit(1)
 
 except ConnectionError:
     logger.error("Can't check eq-translations version")
