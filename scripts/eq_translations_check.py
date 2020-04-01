0f1d110ce45dccaff70d37f91217ad309f84618a
petechd <piotr.chudziak@ons.gov.uk>
2020-04-01 08:40:24 +0100

    Add connection error handling

--- scripts/eq_translations_check.py
+++ scripts/eq_translations_check.py
@@ -2,24 +2,29 @@ import logging
 import requests
 import re
 import sys
+from requests.exceptions import ConnectionError
 
 logger = logging.getLogger(__name__)
 
+try:
+    response = requests.get("https://api.github.com/repos/ONSdigital/eq-translations/commits/master").json()
+    commit_sha = response.get('version')
 
-response = requests.get("https://api.github.com/repos/ONSdigital/eq-translations/commits/master").json()
-commit_sha = response.get('sha')
+    f = open("Pipfile.lock", "r")
+    file = f.readlines()
+    for i, line in enumerate(file):
+        if '"eq-translations"' in line:
+            ref = re.compile(r"\w{40}")
+            mo = ref.search(file[i + 3])
+            pipfile_sha = mo.group()
+            if pipfile_sha == commit_sha:
+                logger.error("eq-translations up to date")
 
-f = open("Pipfile.lock", "r")
-file = f.readlines()
-for i, line in enumerate(file):
-    if '"eq-translations"' in line:
-        ref = re.compile(r"\w{40}")
-        mo = ref.search(file[i + 3])
-        pipfile_sha = mo.group()
-        if pipfile_sha == commit_sha:
-            logger.error("eq-translations up to date")
+            else:
+                logger.error('Newer version of eq-translations available, use "pipenv update"')
+                sys.exit(1)
+            break
 
-        else:
-            logger.error('Newer version of eq-translations available, use "pipenv update"')
-            sys.exit(0)
-        break
+except ConnectionError:
+    logger.error("Can't check eq-translations version")
+    sys.exit(1)
