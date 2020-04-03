bbd3bd64252a2931c9b9070a212e29f4f46efe7d
petechd <piotr.chudziak@ons.gov.uk>
2020-04-03 10:33:08 +0100

    Add requested changes

--- scripts/eq_translations_check.py
+++ scripts/eq_translations_check.py
@@ -7,17 +7,17 @@ import eq_translations
 from requests.exceptions import ConnectionError
 
 logger = logging.getLogger(__name__)
-version = f"v{eq_translations.__version__}"
 
 try:
     response = requests.get(
         "https://api.github.com/repos/ONSdigital/eq-translations/releases"
     )
     if response.status_code == 200:
+        version = eq_translations.__version__
         latest_tag = response.json()[0]["tag_name"]
-        if latest_tag != version:
+        if latest_tag != f"v{version}":
             logger.error(
-                'Newer version of eq-translations available, use "pipenv update"'
+                f'Latest eq-translations version is {version}, use "pipenv update"'
             )
             sys.exit(1)
     else:
