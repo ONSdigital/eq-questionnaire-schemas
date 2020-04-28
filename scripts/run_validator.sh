9a1792ed4a0ddf23ff11740992e68c4d92266185
LJBabbage <lewis.babbage@ons.gov.uk>
2020-04-28 09:19:05 +0100

    revert validator

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-branch=remove-section-summary-block-add-section-end-routing
+branch=latest
 docker pull onsdigital/eq-questionnaire-validator:$branch
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$branch"
