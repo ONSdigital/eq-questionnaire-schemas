f9bd5aa727c1541790c595ffef243b3bb3aa7fea
Mebin Abraham <35296336+MebinAbraham@users.noreply.github.com>
2020-05-18 16:04:43 +0100

    Update scripts/run_validator.sh

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 tag=latest
-docker pull onsdigital/eq-questionnaire-validator:$branch
-docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$branch"
+docker pull onsdigital/eq-questionnaire-validator:$tag
+docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$tag"
