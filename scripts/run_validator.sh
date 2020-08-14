417f856f6b05f03273a829a6b548d601c9928bd1
LJBabbage <lewis.babbage@ons.gov.uk>
2020-08-14 14:18:40 +0100

    Introduce actions for list collectors and block values

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-tag=latest
+tag=add-answer-action-for-lists
 docker pull onsdigital/eq-questionnaire-validator:$tag
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$tag"
