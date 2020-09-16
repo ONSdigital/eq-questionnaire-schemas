fb056bd84b90e6061a2e9fed397cf5bb246eea2e
Rhys Berrow <rhys.berrow@ons.gov.uk>
2020-09-16 12:07:53 +0100

    Update validator branch

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-tag=latest
+tag=add-missing-page-titles
 docker pull onsdigital/eq-questionnaire-validator:$tag
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$tag"
