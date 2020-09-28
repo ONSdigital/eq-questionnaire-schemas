c6c4851a048c32f9ba72c56ab441c75d13687bf3
Ian Wootten <hi@niftydigits.com>
2020-09-28 12:06:02 +0100

    NISRA: Remove individual link from hub

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-tag=latest
+tag=add-flag-for-ir-on-hub
 docker pull onsdigital/eq-questionnaire-validator:$tag
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$tag"
