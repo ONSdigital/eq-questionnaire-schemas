a512ed1de20b9872e45c1840638cbab7a7c8e248
Ian Wootten <hi@niftydigits.com>
2020-09-29 12:03:13 +0100

    Revert branch

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-tag=add-flag-for-ir-on-hub
+tag=latest
 docker pull onsdigital/eq-questionnaire-validator:$tag
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$tag"
