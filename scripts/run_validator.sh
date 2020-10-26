ebddae7923aac3cc1f7ee2845cca094301e3dfab
Mebin Abraham <35296336+MebinAbraham@users.noreply.github.com>
2020-10-26 09:41:55 +0000

    Revert scripts/run_validator.sh

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-tag=add-survey-form-type-region
+tag=latest
 docker pull onsdigital/eq-questionnaire-validator:$tag
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$tag"
