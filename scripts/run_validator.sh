1af25b2d55a4138b3aa7b1647a24d94ebabc74fe
petechd <piotr.chudziak@ons.gov.uk>
2020-11-03 07:56:35 +0000

    Change validator branch

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-tag=latest
+tag=remove-default-checkbox-label
 docker pull onsdigital/eq-questionnaire-validator:$tag
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$tag"
