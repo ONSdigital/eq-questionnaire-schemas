f446ed04edffc23efbab86d1a0a166857aef80f5
petechd <piotr.chudziak@ons.gov.uk>
2021-01-11 15:49:15 +0000

    Revert validator branch

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-tag=lookups-allow-multiple-selection
+tag=latest
 docker pull onsdigital/eq-questionnaire-validator:$tag
 docker run -d -p 5001:5000 "onsdigital/eq-questionnaire-validator:$tag"
